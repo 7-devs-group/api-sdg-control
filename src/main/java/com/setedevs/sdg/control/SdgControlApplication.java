f627fccf07f94e1710227b0edf0296c9a3c99d6d
maddytec <maddytec@gmail.com>
2026-09-14 00:28:21 +0100

    fix: configurar cliente http do control

--- src/main/java/com/setedevs/sdg/control/SdgControlApplication.java
+++ src/main/java/com/setedevs/sdg/control/SdgControlApplication.java
@@ -2,9 +2,16 @@ package com.setedevs.sdg.control;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class SdgControlApplication {
+
+    @Bean
+    RestClient.Builder restClientBuilder() {
+        return RestClient.builder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SdgControlApplication.class, args);
